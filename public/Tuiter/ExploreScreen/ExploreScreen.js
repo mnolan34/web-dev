843dcdc267ae08aea3bfdf69bcea66daddb98477
nolanmatt34 <nolan.matt@northeastern.edu>
2022-02-27 14:44:51 -0500

    Able to site loaded, improper format

--- public/Tuiter/ExploreScreen/ExploreScreen.js
+++ public/Tuiter/ExploreScreen/ExploreScreen.js
@@ -1,6 +1,6 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
-import ExploreComponent from "./ExploreComponent.js/index.js";
-import WhoToFollowList from "../WhoToFollowList.js/index.js";
+import ExploreComponent from "./ExploreComponent.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
 
 (function ($) {
     $('#wd-explore').append(`
@@ -8,7 +8,9 @@ import WhoToFollowList from "../WhoToFollowList.js/index.js";
         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
             ${NavigationSidebar()}
         </div>
+        <div class = "col-6">
             ${ExploreComponent()}
+        </div>
         <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
             ${WhoToFollowList()}
         </div>
