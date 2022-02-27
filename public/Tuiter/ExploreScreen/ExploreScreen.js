13c7badab951eea3ac80bb8972000b07efef7487
nolanmatt34 <nolan.matt@northeastern.edu>
2022-02-27 12:46:34 -0500

    Failure to upload Navbar

--- public/Tuiter/ExploreScreen/ExploreScreen.js
+++ public/Tuiter/ExploreScreen/ExploreScreen.js
@@ -5,14 +5,9 @@ import NavigationSidebar from "../NavigationSidebar/index.js";
 (function ($) {
     $('#wd-explore').append(`
         <div class="row mt-2">
-        <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-            ${NavigationSidebar()}
-        </div>
-            ${ExploreComponent()}
-        <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-            ${WhoToFollowList()}
-        </div>
-        </div>
-     
+            <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+                ${NavigationSidebar()}
+            </div>
+        </div>    
     `);
 })($);
--- public/Tuiter/NavigationSideBar/index.js
+++ public/Tuiter/NavigationSideBar/index.js
@@ -4,9 +4,71 @@ const NavigationSidebar = () => {
                 <a class="list-group-item" href="/">
                     <i class="fab fa-twitter"></i></a>
             <!-- continue the rest of the list -->
+                <a class = "list-group-item" href="../home.html">
+                        <i class="fa fa-home fa-inverse"></i>
+                    <div class = "d-none d-xl-block d-xxl-block">
+                        Home
+                    </div>
+                </a>
+
+                <a class = "list-group-item" href = "../explore.html">
+                    <i class="fa fa-hashtag fa-inverse"></i>
+                    <div class = "d-none d-xl-block d-xxl-block">
+                        Explore
+                    </div>
+                </a>
+ 
+                <a class = "list-group-item" href = "../notifications.html">
+                    <i class="wd-font-black fa fa-bell fa-inverse"></i>
+                    <div class = "wd-big-left wd-font-white d-none d-xl-block d-xxl-block">
+                        Notifications
+                    </div>
+                </a>
+
+                <a class = "list-group-item" href="../messages.html">
+                    <i class="fa fa-envelope fa-inverse"></i>
+                    <div class = "d-none d-xl-block d-xxl-block">
+                        Message
+                    </div>
+                </a>
+
+   
+                <a class = "list-group-item" href = "../bookmarks.html">
+                    <i class="fa fa-bookmark fa-inverse"></i>
+                    <div class = "d-none d-xl-block d-xxl-block">
+                        Bookmarks
+                    </div>
+                </a>
+
+                <a class = "list-group-item" href = "../lists.html">
+                    <i class="fa fa-list fa-inverse"></i>
+
+                    <div class = "d-none d-xl-block d-xxl-block">
+                        List
+                    </div>
+                </a>
+                
+                <a class = "list-group-item" href = "../profile.html">
+                    <i class="fa fa-user fa-inverse"></i>
+
+                    <div class = "d-none d-xl-block d-xxl-block">
+                        Profile
+                    </div>
+                </a>
+
+                <a class = "list-group-item" href = "#">
+                 <span class="me-1 fa-stack fa-1x">
+                        <i class="fas fa-circle fa-fw fa-stack-1x"></i>
+                        <i class="fas fa-ellipsis-h fa-stack-1x"></i>
+                 </span>
+                 <div class = "ms-2 d-none d-xl-block d-xxl-block">
+                     More
+                 </div>
+                </a>
+
             </div>
             <div class="d-grid mt-2">
-                <a href="tweet.html"
+                <a href="../tuit.html"
                    class="btn btn-primary btn-block rounded-pill">
                     Tweet</a>
             </div>
