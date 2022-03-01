c95bb79559a619526e5f830707cee19360bedf6c
nolanmatt34 <nolan.matt@northeastern.edu>
2022-03-01 17:10:55 -0500

    Started route/react labs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
-import './vendors/fontawesome/all.min.css';
+import './vendors/fontawesome/css/all.min.css';
 import './App.css';
+import HelloWorld from "./components/HelloWorld";
+import Labs from "./components/Lab";
+import Tuiter from "./components/Tuiter";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
-      <div className = "container">
-          <h1>Hello World!</h1>
-      </div>
+      <BrowserRouter>
+          <div className = "container">
+              <Route path="/hello">
+                <HelloWorld/>
+              </Route>
+              <Route path="/labs">
+                <Labs/>
+              </Route>
+              <Route path="/tuiter">
+                <Tuiter/>
+              </Route>
+          </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/Lab.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Lab = () => {
+    return(
+        <h1>Lab</h1>
+    )
+};
+
+export default Lab;
