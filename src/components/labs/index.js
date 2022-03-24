16f8bccced5080a45a572d64c2d876796539364a
nolanmatt34 <nolan.matt@northeastern.edu>
2022-03-23 22:28:15 -0400

    Lab 7 Hello World and ToDo, installed react router dom 6.2.2

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -1,28 +1,13 @@
 import React from "react";
-import Classes from "./classes/";
-import Styles from "./styles/index.js";
-import ConditionalOutput from "./ConditionalOutput/index.js";
-import ToDo from "./ToDo/TodoList";
-import {Link} from "react-router-dom";
+import ReduxExamples from "./redux-examples"
 
-
-const Index = () => {
+const Labs = () => {
     return(
         <div>
             <h1>Labs</h1>
-            <ConditionalOutput/>
-            <Styles/>
-            <Classes/>
-            <ToDo/>
-
-            <Link to="/hello">
-                Hello
-            </Link> |
-            <Link to="/tuiter/explore">
-                Tuiter
-            </Link>
+            <ReduxExamples/>
         </div>
     )
 };
 
-export default Index;
+export default Labs;
