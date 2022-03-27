e8383ba70d26ffc8bde90a3e4ffcb123394b44f0
nolanmatt34 <nolan.matt@northeastern.edu>
2022-03-26 20:36:49 -0400

    Who To Follow List implemented

--- src/components/tuiter/WhoToFollowList/index.js
+++ src/components/tuiter/WhoToFollowList/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import who from "../data/who.json";
+//import who from "../data/who.json";
+import {useSelector} from "react-redux";
 import WhoToFollowListItem from "./WhoToFollowListItem";
 const WhoToFollowList = () => {
+    const who = useSelector((state) => state);
     return (
             <ul className="list-group">
                 <div className = "list-group-item">
--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,20 +1,29 @@
-import React from "react";
 import {Outlet} from "react-router-dom";
-
-import NavigationSidebar from "./NavigationSideBar/index";
-import WhoToFollowList from "./WhoToFollowList/WhoToFollowListItem";
-import PostSummaryItem from "./PostSummaryList/PostSummaryItem";
-import ExploreComponent from "./ExploreScreen/ExploreComponent";
+import NavigationSidebar from "./NavigationSideBar";
+import WhoToFollowList
+    from "./WhoToFollowList";
+import "./ExploreScreen/explore.css";
+import "./HomeScreen/bookmarks.css";
+import whoReducer from "./reducers/who-reducer";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+const store = createStore(whoReducer);
 
 const Tuiter = () => {
-    return(
-        <>
-            <NavigationSidebar active="home"/>
-            <WhoToFollowList/>
-            <ExploreComponent/>
-            <PostSummaryItem/>
-        </>
-)
+    return (
+        <Provider store={store}>
+            <div className="row mt-2">
+                <div className="col-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar/>
+                </div>
+                <div className="col-10 col-lg-7 col-xl-6">
+                    <Outlet/>
+                </div>
+                <div className="d-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList/>
+                </div>
+            </div>
+        </Provider>
+    );
 };
-
 export default Tuiter;
