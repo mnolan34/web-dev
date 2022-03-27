56b02d93f492e3f48733478de39ea381bb87634e
nolanmatt34 <nolan.matt@northeastern.edu>
2022-03-27 16:40:30 -0400

    Rough Draft profileComponent

---
+++ src/components/tuiter/ProfileScreen/profileComponent.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import { useDispatch } from 'react-redux';
+
+const Profile = ({profile}) => {
+    const dispatch = useDispatch();
+    const editProfile = (profile) => {
+        dispatch({type: 'edit-profile', profile})
+
+        return (
+            <div className="wd-suggested-post">
+                <img className="wd-avatar-photo"
+                     src={profile.profilePicture}
+                     alt="avatar"/>
+
+                <div className="wd-tweet-info-pos wd-author-font">
+                    {profile.firstName} {profile.lastName}
+                    <span className="ms-1 wd-handle-font">
+                     @{profile.handle}
+                    </span>
+                </div>
+
+                <div className="float-end">
+                    <button onClick={() =>
+                        editProfile(profile)}
+                            className="btn btn-primary"> Edit Profile
+                    </button>
+                </div>
+
+                <div className="mt-0 wd-body-text wd-tweet-info-pos">
+                    {profile.location} {profile.dateOfBirth} {profile.dateJoined}
+                </div>
+
+                <div className="wd-author-font wd-icon-color wd-stats-container">
+
+                    <div className="wd-heart-color">
+                        <Link className="wd-heart-color" to="#">
+                            <i className="fa fa-heart"></i> {profile.following} Following
+                        </Link>
+                    </div>
+
+                    <div>
+                        <Link className="wd-icon-color" to="#">
+                            <i className="fa fa-share">
+                                {profile.followers} Followers
+                            </i>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default TuitListItem;
