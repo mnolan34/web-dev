5c8ccfb3cadf17ccf9e7663d869c7ef67a1cd57c
nolanmatt34 <nolan.matt@northeastern.edu>
2022-03-28 11:48:49 -0400

    Updated Whats-Happening, tuit button still off

--- src/components/tuiter/whatsHappening/whatsHappening.js
+++ src/components/tuiter/whatsHappening/whatsHappening.js
@@ -10,44 +10,45 @@ const WhatsHappening = () => {
         tuit: whatsHappening});
     }
     return (
-        <>
-            <div className = "wd-flex-container wd-suggested-post">
-                <img className = "wd-avatar-photo"
-                     src = "../../../images/nasa.png"
-                     alt = "avatar"/>
+        <div className="wd-suggested-post">
+            <img className ="wd-avatar-photo"
+                 src = "../../../images/react-blue.png"
+                 alt = "avatar"/>
 
-                <div>
-                  <textarea className = "wd-body-text float-end p-1 wd-input-bar"
-                            placeholder = "What's happening?"
-                      value={whatsHappening}
-                            onChange={(event) =>
-                                setWhatsHappening(event.target.value)}>
+
+            <div className ="mt-0 wd-body-text wd-tweet-info-pos">
+                 <textarea className = "wd-body-text float-end p-1 wd-input-bar"
+                           placeholder = "What's happening?"
+                           value={whatsHappening}
+                           onChange={(event) =>
+                               setWhatsHappening(event.target.value)}>
                   </textarea>
-                </div>
-
-                <div>
-                    <div>
-                        <i className = "fa fa-file-photo-o"></i>
-                    </div>
-                    <div>
-                        <i className = "fa fa-line-chart"></i>
-                    </div>
-                    <div>
-                        <i className = "fa fa-smile-o"></i>
-                    </div>
-                    <div>
-                        <i className = "fa fa-calendar-o"></i>
-                    </div>
-
-                    <div>
-                        <button className = "btn btn-primary float-end width = 100%"
-                            onClick={tuitClickHandler}>
-                            Tuit
-                        </button>
-                    </div>
-                </div>
             </div>
-        </>
+
+
+
+                <div className ="wd-icon-color wd-whatsHappening-container">
+                    <div>
+                        <i className = "pe-3 fa fa-file-image"></i>
+                    </div>
+                    <div>
+                        <i className = "pe-3 fa fa-line-chart"></i>
+                    </div>
+                    <div>
+                        <i className = "pe-3 fa fa-smile"></i>
+                    </div>
+                    <div>
+                        <i className = "fa fa-calendar"></i>
+                    </div>
+
+                    <button className = "float-right btn btn-primary"
+                                onClick={tuitClickHandler}>
+                                Tuit
+                    </button>
+                </div>
+
+
+            </div>
     );
 }
 export default WhatsHappening;
