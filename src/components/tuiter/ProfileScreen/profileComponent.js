636579a8d528dc5df31480847f89a0c7e52e3713
nolanmatt34 <nolan.matt@northeastern.edu>
2022-03-28 18:43:44 -0400

    Restarted Profile Component

--- src/components/tuiter/ProfileScreen/profileComponent.js
+++ src/components/tuiter/ProfileScreen/profileComponent.js
@@ -1,48 +1,20 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import { useDispatch } from 'react-redux';
+import profile from "../data/profiles.json"
 
-const ProfileComponent = ({profile}) => {
-    const dispatch = useDispatch();
-    const editProfile = () => {
-        dispatch({type: 'edit-profile', profile});
-    };
+const ProfileComponent = (profile) => {
+    return(
+        <div className="list-group-item">
+            <img className="wd-avatar-photo"
+                 src={profile.profilePhoto}
+                 alt = "avatar"/>
+            <div className="wd-float-left">
+                <div className="wd-font-white ms-1"> {profile.userName} <i className="fa fa-check-circle"></i></div>
+                <div className="wd-font-gray ms-1"> @{profile.handle} </div>
+            </div>
 
-    return (
-                <div className="wd-suggested-post">
-                    <img className="wd-avatar-photo"
-                         src={profile.profilePicture}
-                         alt="avatar"/>
+            <button className="btn btn-primary wd-override-follow">
+                Follow
+            </button>
+        </div>
+    )}
 
-                    <div className="wd-tweet-info-pos wd-author-font">
-                        {profile.firstName} {profile.lastName}
-                        <span className="ms-1 wd-handle-font">
-                         @{profile.handle}
-                        </span>
-                    </div>
-
-
-                    <div className="mt-0 wd-body-text wd-tweet-info-pos">
-                        {profile.location} {profile.dateOfBirth} {profile.dateJoined}
-                    </div>
-
-                    <div className="wd-author-font wd-icon-color wd-stats-container">
-
-                        <div className="wd-heart-color">
-                            <Link className="wd-heart-color" to="#">
-                                <i className="fa fa-heart"></i> {profile.following} Following
-                            </Link>
-                        </div>
-
-                        <div>
-                            <Link className="wd-icon-color" to="#">
-                                <i className="fa fa-share">
-                                    {profile.followers} Followers
-                                </i>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-        );
-}
 export default ProfileComponent;
