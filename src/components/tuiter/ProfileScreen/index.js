35ac2eb9d953c4bc9aa9646e49bcc257427ee39e
nolanmatt34 <nolan.matt@northeastern.edu>
2022-03-28 18:17:40 -0400

    Added Edit Profile stuff

--- src/components/tuiter/ProfileScreen/index.js
+++ src/components/tuiter/ProfileScreen/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import {useSelector} from "react-redux";
 import ProfileComponent from "./profileComponent";
 import '../HomeScreen/bookmarks.css';
 import '../ExploreScreen/explore.css';
 
 const ProfileList = () => {
+    const [profile, setProfile] = useState(profile);
     const profiles = useSelector(
         state => state.profiles);
     return (
