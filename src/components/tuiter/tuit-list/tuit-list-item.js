40847d8673d91faaf4a170d6e313ebba7a467f35
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-05 13:39:32 -0400

    Fixed Update Tuit in tuit-list-item

--- src/components/tuiter/tuit-list/tuit-list-item.js
+++ src/components/tuiter/tuit-list/tuit-list-item.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {createTuit, deleteTuit, findAllTuits} from "../../actions/tuits-action";
+import {createTuit, deleteTuit, findAllTuits, updateTuit} from "../../actions/tuits-action";
 import {Link} from "react-router-dom";
 import { useDispatch } from 'react-redux';
 
