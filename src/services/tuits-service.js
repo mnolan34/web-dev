8164f6ced68d95d1593cc9155ef183af04526673
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-05 17:50:52 -0400

    Added /api/ to base of TUITS_API for proper heroku link

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //const TUITS_API = 'https://localhost:4000/api/tuits';
 //const TUITS_API = 'https://wdev-server-node.herokuapp.com';
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = `${API_BASE}/api/tuits`;
 
 export const createTuit = async(tuit) => {
     const response = await axios.post(TUITS_API, tuit)
